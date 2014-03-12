d0fde1652002c783cd4ac6c335b79bae06b54e81
Fabrice Bacchella <fbacchella@spamcop.net>
2014-03-12 11:28:18 +0100

    addvolume can now auto extend a given lvm

--- oslib/instance/newvolume.py
+++ oslib/instance/newvolume.py
@@ -6,6 +6,7 @@ import re
 import time
 from oslib.command import Command
 from boto.ec2.blockdevicemapping import BlockDeviceMapping, BlockDeviceType
+import subprocess
 
 device_re = re.compile('/dev/sd(.)')
 
@@ -20,8 +21,13 @@ This class is used to associate an EIP to an instance"""
     def fill_parser(self, parser):
         parser.add_option("-n", "--name", dest="name", help="New volume name", default=None)
         parser.add_option("-s", "--size", dest="size", help="New volume size (in GB)", default=None)
+        parser.add_option("-m", "--mount", dest="mount", help="mount point to attach and extend", default='')
+        parser.add_option("-d", "--deviceid", dest="deviceid", help="device id used by blkid", default='')
 
     def execute(self, *args, **kwargs):
+        if not kwargs['name']:
+            yield "Missing volume name"
+            raise StopIteration 
         instance = self.ec2_object.get()
         last = ord('f')
         for device in instance.block_device_mapping:
@@ -29,20 +35,42 @@ This class is used to associate an EIP to an instance"""
             if device_match: 
                 last = max(last, ord(device_match.group(1)))
         
-        volume = self.conn.create_volume(kwargs['size'], instance.placement)
+        volume = self.ctxt.cnx_ec2.create_volume(kwargs['size'], instance.placement)
         while volume.status != 'available':
             volume.update(True)
             yield (".")
             time.sleep(1)
         yield ("\n")
-        self.conn.attach_volume(volume.id, instance.id, "/dev/sd%s" % chr(last + 1))
+        self.ctxt.cnx_ec2.attach_volume(volume.id, instance.id, "/dev/sd%s" % chr(last + 1))
         tags ={}
         user = getuser.user
         tags['creator'] = user
         tags['Name'] = instance.tags['Name'] + '/' + kwargs['name']
         tags['instance'] = instance.tags['Name']
-        self.conn.create_tags([ volume.id ], tags)
+        self.ctxt.cnx_ec2.create_tags([ volume.id ], tags)
+        while volume.status != 'in-use':
+            volume.update(True)
+            yield (".")
+            time.sleep(1)
+        yield ("\n")
+        if kwargs['mount'] or kwargs['deviceid']:
+            key_file = self.ctxt.key_file
+            remote_user = self.ctxt.user
+            root_embedded = oslib.resources.__path__[0]
+            extendvol = open(os.path.join(root_embedded, "extendvol"), 'r')
+            # "'%s'" % value is needed to keep empty value
+            # anyway ssh eat it
+            args=["ssh", "-o", "GSSAPIAuthentication=no", "-tt", "-o", "UserKnownHostsFile=/dev/null",  "-x", "-o", "StrictHostKeyChecking=no", "-i", key_file,  "-l", remote_user, instance.public_dns_name, "sudo", "bash", "-s", volume.attach_data.device, "'%s'" % kwargs['mount'], "'%s'" % kwargs['deviceid'] ]
+            print args
+            try:
+                subprocess.check_call(args, shell=False, stdin=extendvol)
+            except subprocess.CalledProcessError as e:
+                raise oslib.OSLibError(e)
+            extendvol.close()
         yield volume
         
     def to_str(self, volume):
-        return volume
+        if volume.__class__ == "".__class__ or volume.__class__ == u"".__class__:
+            return volume
+        else:
+            return "created volume %s as %s" % (volume.id, volume.attach_data.device)
--- oslib/volume/dump.py
+++ oslib/volume/dump.py
@@ -10,6 +10,8 @@ class Dump(oslib.command.Dump):
     status: %s
     snapshot_id: %s
 """ % (volume.id, volume.size, volume.status, volume.snapshot_id)
+        if volume.status == 'in-use':
+            status += "    device: %s\n" % volume.attach_data.device
 
         if len(volume.tags.keys()) > 0:
             status += "    tags =\n"
