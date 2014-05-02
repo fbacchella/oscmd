8cb879fee61cc6505c981df593fcd3ccc6d74513
Fabrice Bacchella <fbacchella@spamcop.net>
2014-05-02 16:28:50 +0200

    A check for instance state with ssh

--- oslib/instance/ssh.py
+++ oslib/instance/ssh.py
@@ -21,6 +21,8 @@ Argument can be given to ssh after a --"""
             return
         if not 'user' in kwargs or not kwargs['user']:
             kwargs['user'] = self.ctxt.user
+        if not instance.public_dns_name or instance.state != 'running':
+            raise oslib.OSLibError("connecting to an invalid state instance")
         ssh_args=["ssh", "-o", "UserKnownHostsFile=/dev/null",  "-x", "-o", "StrictHostKeyChecking=no", "-i", self.ctxt.key_file,  "-l", kwargs['user'], instance.public_dns_name]
         if len(args) > 0:
             ssh_args.extend(args[0])
