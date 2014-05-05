07bafc3f9770bc862a9adbb23f075010857d6e21
Fabrice Bacchella <fbacchella@spamcop.net>
2014-05-05 17:25:18 +0200

    - string output moved to oscmd, not part of oslib - run_phrase can throws exception again, it's not it's duty to manage them.

--- oslib/__init__.py
+++ oslib/__init__.py
@@ -1,7 +1,3 @@
-import boto
-import codecs
-import locale
-import optparse
 import sys
 
 all_libs = (
@@ -41,41 +37,6 @@ def get_cmd(context, ec2_object, verb):
     # Everything failed so return false
     return False
 
-# needed because default unicode to ascii encoding is strict
-# we will use replace instead
-output_locale = locale.getpreferredencoding()
-
-def unicodetoascii(string):
-    return codecs.encode(string, output_locale, 'replace')
-
-def print_run_phrase(context, ec2_object, verb, args=None, **kwargs):
-    (cmd, executed) = run_phrase(context, ec2_object, verb, args, **kwargs)
-    if cmd == None:
-        print "invalid phrase '%s %s'" % (ec2_object.name, verb)
-        return 255
-    # If execute return a generator, iterate other it
-    if executed.__class__.__name__ == 'generator':
-        for s in executed:
-            if s != None:
-                string = cmd.to_str(s)
-                if string:
-                    print codecs.encode(string, output_locale, 'replace'),
-                    sys.stdout.flush()
-        return cmd.status()
-    # An exception was catched, print it
-    elif isinstance(executed, Exception):
-        print "'%s %s' failed with \"%s\"" % (ec2_object.name, verb, executed.error_message)
-    # Else if it return something, just print it
-    elif executed != None and executed:
-        string = cmd.to_str(executed)
-        if string:
-            print unicodetoascii(string),
-        return cmd.status()
-    #It return false, something went wrong
-    elif executed != None:
-        print "'%s %s' failed" % (ec2_object.name, verb)
-    return 255
-
 def run_phrase(context, ec2_object, verb, args=None, **kwargs):
     cmd = get_cmd(context, ec2_object, verb)
     if cmd:
@@ -84,28 +45,19 @@ def run_phrase(context, ec2_object, verb, args=None, **kwargs):
             verb_options = vars(verb_options)
         else:
             (verb_options, verb_args) = (kwargs, [])
-        try:
-            if cmd.validate(args):            
-                return (cmd, cmd.execute(verb_args, **verb_options))
-        except (boto.exception.EC2ResponseError, OSLibError) as e:
-            return (cmd, e)
+        if cmd.validate(args):            
+            return (cmd, cmd.execute(verb_args, **verb_options))
     # Nothing done, return nothing
     return (None, None)
     
 for lib in all_libs:
-#    try:
-            cmd_module = __import__(lib, globals(), locals(), [], -1)
-#        try:
-            for class_ref in cmd_module.class_ref:
-                try:
-                    verb_name = class_ref.verb
-                    object_name = class_ref.object
-                    if not object_name in objects:
-                        objects[object_name] = {}
-                    objects[object_name][verb_name] = class_ref
-                except:
-                    print "Invalid verb:", sys.exc_info()[0]
-#        except:
-#            print "Invalid lib:", sys.exc_info()[0]
-#    except AttributeError as e:
-#        print "%s is invalid action" % lib
+    cmd_module = __import__(lib, globals(), locals(), [], -1)
+    for class_ref in cmd_module.class_ref:
+        try:
+            verb_name = class_ref.verb
+            object_name = class_ref.object
+            if not object_name in objects:
+                objects[object_name] = {}
+            objects[object_name][verb_name] = class_ref
+        except:
+            print "Invalid verb:", sys.exc_info()[0]
