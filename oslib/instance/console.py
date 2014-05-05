3471c3d3c88cb86f19f73aec67ae3d818a5eec16
Fabrice Bacchella <fbacchella@spamcop.net>
2014-05-05 17:23:50 +0200

    Improved console following

--- oslib/instance/console.py
+++ oslib/instance/console.py
@@ -1,13 +1,13 @@
 import boto
 import codecs
-from oslib.command import Command
+from oslib.command import Command, OSLibError
 import time
-import zlib
 import string
+import difflib
 
 class Console(Command):
-    """
-This class is used to display the console of an existing instance"""
+    """This class is used to display the console of an existing instance
+"""
     object = 'instance'
     verb = 'console'
 
@@ -20,47 +20,34 @@ This class is used to display the console of an existing instance"""
 
 
     def execute(self, *args, **kwargs):
+        console_output = self.ec2_object.get().get_console_output()
+        #A bug in outscale API, needs to skip time == 0
+        if console_output.timestamp == '1970-01-01T00:00:00.000Z':
+            raise OSLibError("broken console, stop/start the instance")
+        yield console_output.output
+
         if not kwargs['follow']:
-            console_output = self.ec2_object.get().get_console_output()
-            if console_output.timestamp == '1970-01-01T00:00:00.000Z':
-                yield "broken console, stop/start the instance"
-                return
-            for l in console_output.output.split('\n'):
-                yield l
             return
 
-        last_timestamp = None
-        last_digest = None
-        depth_search = 10
+        last_timestamp = console_output.timestamp
+        last_buffer = console_output.output
 
         while True:
             line_found = False
             console_output = self.ec2_object.get().get_console_output()
-            if console_output.timestamp == '1970-01-01T00:00:00.000Z':
-                yield "broken console, stop/start the instance"
-                return
-            #A bug in outscale API, needs to skip time == 0
             if console_output.timestamp == last_timestamp or not console_output.output or len(console_output.output) == 0:
                 time.sleep(1)
                 continue
-            lines = console_output.output.split('\n')
-            i = len(lines) - 1
-            while i >= 0 and depth_search != 0 :
-                l = lines[i]
-                line_digest = zlib.adler32(l)
-                if last_digest and last_digest ==  line_digest:
-                    #skip this line
-                    i += 1
-                    break
-                i -= 1
-                depth_search -= 1
-            for l in lines[max(i,0):]:
-                yield l
-                if len(l) > 0:
-                    last_non_empty = l
-            depth_search = -1
-            last_digest = zlib.adler32(last_non_empty)
+            differ = difflib.SequenceMatcher(a=last_buffer, b=console_output.output)
+            matched =  differ.get_matching_blocks()
+            #print matched
+            # SequenceMatcher add an empty match at the sequence's end
+            # check if the sequence make sence, skip it otherwise
+            if(len(matched) == 2 and matched[0].b == 0 and matched[1].size == 0):
+                yield console_output.output[matched[0].size:]
             last_timestamp = console_output.timestamp
+            last_buffer = console_output.output
 
     def to_str(self, line):
-        return codecs.decode(line, 'ascii', 'replace') + "\n"
+        #Strange things can appen on a console, don't fail on them
+        return codecs.decode(line, 'ascii', 'replace')
