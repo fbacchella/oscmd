2a7752591ffb0c819026a15181d8115d8453b7bf
Fabrice Bacchella <fbacchella@spamcop.net>
2014-06-02 19:37:49 +0200

    improved securitygroup dump output

--- oslib/securitygroup/dump.py
+++ oslib/securitygroup/dump.py
@@ -4,14 +4,36 @@ class Dump(oslib.command.Dump):
     object = 'securitygroup'
     verb = 'dump'
 
+    def fill_parser(self,parser):
+        parser.add_option("-u", "--used", dest="used", help="Only used security group", default=False, action='store_true')
+        
+    def execute(self, *args, **kwargs):
+        only_used = kwargs.pop('used')
+        for sg in super(Dump, self).execute(*args, **kwargs):
+            instances =  sg.instances()
+            sg.instances = instances
+            if (not only_used) or (only_used and len(instances) > 0) :
+                yield sg
+        
     def to_str(self, sg):
         status = "==== %s ====\n" % sg.id
         status += "    name: %s\n" % sg.name
         status += "    owner_id: %s\n" % sg.owner_id
         status += "    description: %s\n" % sg.description
+        status += "    used by: [ %s ]\n" % reduce(lambda x, y: "%s, %s" % (x, y.id) , sg.instances, "")[2:]
         status += "    rules:\n"
         for permissions in sg.rules:
-            status += "        %s\t%s\t%s\n" % (permissions.ip_protocol, permissions.from_port, permissions.to_port)
+            from_port = int(permissions.from_port)
+            to_port = int(permissions.to_port)
+            if permissions.ip_protocol == 'icmp' and from_port == -1 and to_port == -1:
+                port_range = 'ALL'
+            elif from_port == 0 and to_port == 65535:
+                port_range = 'ALL'
+            elif from_port == to_port:
+                port_range = permissions.from_port
+            else:
+                port_range = "%s-%s" % ( permissions.from_port, permissions.to_port )
+            status += "        % 4s/%s\n" % ( permissions.ip_protocol, port_range )
             for grant in permissions.grants:
                 status +=  "            %s\n" % (grant)
         return status
