ea7d6fb80d0d7a56b234fcf60e4cc6bd0f2c1f12
Fabrice Bacchella <fbacchella@spamcop.net>
2015-04-10 14:29:28 +0200

    Improved instance's ssh sub command, take a key file path

--- oslib/instance/ssh.py
+++ oslib/instance/ssh.py
@@ -1,6 +1,8 @@
 import oslib
-from oslib.command import Command
+from oslib.command import Command, OSLibError
 import subprocess
+import os.path
+
 
 class SSH(Command):
     """
@@ -12,18 +14,24 @@ Argument can be given to ssh after a --"""
     verb = 'ssh'
 
     def fill_parser(self, parser):
-        parser.add_option("-u", "--user", dest="user", help="login user")
+        parser.add_option("-u", "--user", dest="user", help="login user", default=self.ctxt.user)
+        parser.add_option("-f", "--key_file", dest="key_file", help="key file", default=self.ctxt.key_file)
 
     def execute(self, *args, **kwargs):
         instance = self.ec2_object.get()
-        if not self.ctxt.key_file:
-            print "no private key file"
-            return
-        if not 'user' in kwargs or not kwargs['user']:
-            kwargs['user'] = self.ctxt.user
+
+        key_file = kwargs['key_file']
+        if key_file is None or not os.path.exists(key_file):
+            raise OSLibError("no accessible private key file: '%s'" % key_file)
+
+        user = kwargs['user']
+        if user is None:
+            raise OSLibError("no defined user to user for the connection")
+
         if not instance.public_dns_name or instance.state != 'running':
             raise oslib.OSLibError("connecting to an invalid state instance")
-        ssh_args=["ssh", "-o", "UserKnownHostsFile=/dev/null",  "-x", "-o", "StrictHostKeyChecking=no", "-i", self.ctxt.key_file,  "-l", kwargs['user'], instance.public_dns_name]
+        ssh_args = ["ssh", "-o", "UserKnownHostsFile=/dev/null",  "-x", "-o", "StrictHostKeyChecking=no",
+                    "-i", key_file,  "-l", user, instance.public_dns_name]
         if len(args) > 0:
             ssh_args.extend(args[0])
         subprocess.call(ssh_args)
